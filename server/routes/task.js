7987d5af42ac85f07193f706164d2551bde922b4
Peter Stockings <pstockings@jadeworld.com>
2021-05-27 11:33:03 +1000

    Complete refactor of task API

--- server/routes/task.js
+++ server/routes/task.js
@@ -4,13 +4,32 @@ let Task = require("../models/task.model");
 
 // Get all tasks
 router.get("/", (req, res, next) => {
+
   Task.find()
-  .then(tasks => res.json(tasks))
-  .catch(next)
+    .then(tasks => res.json(tasks))
+    .catch(next)
+});
+
+// Create new task
+router.route("/").post((req, res) => {
+
+  const newTask = new Task({
+    collection_id: req.body.collection_id,
+    name: req.body.name,
+    description: req.body.description,
+    deadline: Date.parse(req.body.deadline),
+    completed: false,
+  });
+
+  newTask
+    .save()
+    .then(task => res.json(task))
+    .catch((err) => res.status(400).json(err));
 });
 
 // Get task by id
 router.route("/:id").get((req, res, next) => {
+
   Task.findById(req.params.id)
     .then((task) => res.json(task))
     .catch(next)
@@ -18,20 +37,22 @@ router.route("/:id").get((req, res, next) => {
 
 // Delete all tasks
 router.route("/").delete((req, res, next) => {
+
   Task.deleteMany({})
     .then(() => res.json({message: 'Successfully deleted all tasks'}))
     .catch(next)
 });
 
-//Delete task by id
+// Delete task by id
 router.route("/:id").delete((req, res, next) => {
+
   Task.findByIdAndDelete(req.params.id)
     .then((deletedTask) => res.json(deletedTask))
     .catch(next);
 });
 
+// Update task by id
 router.route("/update/:id").put((req, res, next) => {
-  console.log(`PUT /update/${req.params.id}`)
 
   Task.findByIdAndUpdate(req.params.id)
     .then((task) => {
@@ -40,14 +61,15 @@ router.route("/update/:id").put((req, res, next) => {
       task.description = req.body.description;
       task.completed = req.body.completed;
       task.deadline = Date.parse(req.body.deadline);
-      task
-        .save()
-        .then(() => res.json("Task updated!"))
-        .catch(next);
+
+      task.save()
+        .then(task => res.json(task))
+        .catch(err => res.status(400).json(err));
     })
     .catch(next);
 });
 
+// Complete task by id
 router.route("/complete/:id").put((req, res, next) => {
 
   Task.findByIdAndUpdate({_id: req.params.id}, {completed: true})
@@ -55,30 +77,12 @@ router.route("/complete/:id").put((req, res, next) => {
     .catch(next)
 });
 
+// Open task by id
 router.route("/open/:id").put((req, res, next) => {
 
   Task.findByIdAndUpdate({_id: req.params.id}, {completed: false})
     .then((task) => res.json(task))
     .catch(next)
-});
-
-router.route("/").post((req, res) => {
-  const collection_id = req.body.collection_id;
-  const name = req.body.name;
-  const description = req.body.description;
-  const deadline = Date.parse(req.body.deadline);
-  const completed = false;
-  const newTask = new Task({
-    collection_id,
-    name,
-    description,
-    deadline,
-    completed,
-  });
-  newTask
-    .save()
-    .then(() => res.json("Task Added"))
-    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
